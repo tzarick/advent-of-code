a62c4a62731ca9affae534fb02a619d7625e5579
Tommy Zarick <zarick.6@osu.edu>
2021-12-28 01:14:55 -0500

    day 8 part 2 working

--- 2021/day08/solution/solution.go
+++ 2021/day08/solution/solution.go
@@ -1,89 +1,110 @@
 package solution
 
 import (
+	"log"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type display struct {
-	signals                    []string
-	output                     []string
-	customSegmentToStdSegments map[byte]int // map the letter we get to the std segment (key below)
-}
-
-const (
-	top         = iota
-	topLeft     = iota
-	topRight    = iota
-	mid         = iota
-	bottomLeft  = iota
-	bottomRight = iota
-	bottom      = iota
-)
-
-// state of all 7 segments -> displayed digit
-var stdSegmentComboToDigitMap map[string]int = map[string]int{
-	"1110111": 0,
-	"0010010": 1,
-	"1011101": 2,
-	"1011011": 3,
-	"0111010": 4,
-	"1101011": 5,
-	"1101111": 6,
-	"1010010": 7,
-	"1111111": 8,
-	"1111011": 9,
+	signals []string
+	output  []string
 }
 
 func Run(rawList string) (part1, part2 int) {
 	displayInputs := parseInput(rawList)
 
-	totalOutputVal := 0
+	var totalOutputVal, totalEasyNumCount int
+
 	for i := range displayInputs {
-		customSegmentToStdSegments := decodeSegmentLines(displayInputs[i])
-		totalOutputVal += decodeOutput(displayInputs[i].output, customSegmentToStdSegments)
+		seqToNumMap := decodeSegmentLines(displayInputs[i])
+		outputVal, easyNumCount := decodeOutput(displayInputs[i].output, seqToNumMap)
+
+		totalOutputVal += outputVal
+		totalEasyNumCount += easyNumCount
 	}
 
-	return getUniqueOutputDigitCount(displayInputs), totalOutputVal
+	return totalEasyNumCount, totalOutputVal
 }
 
-func getUniqueOutputDigitCount(displays []display) int {
-	var uniqueCount int
-	for _, display := range displays {
-		for _, digit := range display.output {
-			if d := len(digit); d == 2 || d == 3 || d == 4 || d == 7 {
-				uniqueCount++
+func decodeSegmentLines(d display) map[string]int {
+	numberMap := make(map[int]string)
+
+	// push the harder-to-determine segment sequences to the back. These sequeunces are any with length 5 and 6
+	sort.Slice(d.signals, func(i, j int) bool {
+		return len(d.signals[i]) != 5 && len(d.signals[i]) != 6
+	})
+
+	for _, signal := range d.signals {
+		switch len(signal) {
+		case 2:
+			numberMap[1] = sortAlpha(signal)
+		case 3:
+			numberMap[7] = sortAlpha(signal)
+		case 4:
+			numberMap[4] = sortAlpha(signal)
+		case 7:
+			numberMap[8] = sortAlpha(signal)
+
+		// harder to determine combos:
+		// Decode these number values by subtracting the number segments we know (above) from the unknown segments and use the unique differences to our advantage
+		case 5:
+			// sequences with len 5:
+			// it's a 3 if: unknownSequence - numberMap[7] = 2 segments left
+			// it's a 2 if: unknownSequence - numberMap[4] = 3 segments left
+			// it's a 5 if: doesn't satisfy the other len 5 cases ^
+
+			if len(subtractSequences(signal, numberMap[7])) == 2 {
+				numberMap[3] = sortAlpha(signal)
+			} else if len(subtractSequences(signal, numberMap[4])) == 3 {
+				numberMap[2] = sortAlpha(signal)
+			} else {
+				numberMap[5] = sortAlpha(signal)
+			}
+
+		case 6:
+			// sequences with len 6:
+			// it's a 9 if: unknownSequence - numberMap[4] = 2 segments left
+			// it's a 6 if: unknownSequence - numberMap[1] = 5 segments left
+			// it's a 0 if: doesn't satisfy the other len 6 cases ^
+
+			if len(subtractSequences(signal, numberMap[4])) == 2 {
+				numberMap[9] = sortAlpha(signal)
+			} else if len(subtractSequences(signal, numberMap[1])) == 5 {
+				numberMap[6] = sortAlpha(signal)
+			} else {
+				numberMap[0] = sortAlpha(signal)
 			}
 		}
 	}
 
-	return uniqueCount
+	return reverseMap(numberMap) // reverse so when we use it for decoding we can go from sequence -> number val
 }
 
-func decodeSegmentLines(d display) map[byte]int {
-	// fill d.customSegmentToStdSegments
-	// for _, digit := range d.output {
+// returns:
+// totalOutput: The output sequencec numbers decoded and concatenated as a 4 digit number
+// easyNumCount: The number of occurrences of the numbers 1, 4, 7, and 8 in the output sequence
+func decodeOutput(sequence []string, seqToNumMap map[string]int) (totalOutput, easyNumCount int) {
 
-	// }
-	return map[byte]int{}
-}
-
-func decodeOutput(sequence []string, key map[byte]int) int {
 	var decodedSeq []byte
 	for _, seq := range sequence {
-		num := decodeNum(seq, key)
+		num := seqToNumMap[sortAlpha(seq)]
+
+		if num == 1 || num == 4 || num == 7 || num == 8 {
+			easyNumCount++
+		}
+
 		numString := strconv.Itoa(num)
 		decodedSeq = append(decodedSeq, []byte(numString)...)
 	}
 
-	decodedNum, _ := strconv.Atoi(string(decodedSeq))
+	decodedNum, err := strconv.Atoi(string(decodedSeq))
+	if err != nil {
+		log.Fatalf("error parsing string -> number: %v", err)
+	}
 
-	return decodedNum
-}
-
-// decode a single input of a single segment sequence and return the number 0-9 that corresponds to it based on the custom mapping
-func decodeNum(input string, key map[byte]int) int {
-	return 0
+	return decodedNum, easyNumCount
 }
 
 func parseInput(list string) []display {
@@ -104,23 +125,42 @@ func parseInput(list string) []display {
 	return cleaned
 }
 
-// rand comb -> number
-// rand comb -> decoded mapping -> segment key -> std map -> number
+// helpers
 
-// segment key
-//
-// top
-// t  t
-// o  o
-// p  p
-// l  r
-// t  t
-// mid
-// b  b
-// o  o
-// t  t
-// l  r
-// t  t
-// bottom
+func subtractSequences(a, b string) []rune {
+	var diff []rune
 
-// top = 0, toplt = 1, toprt = 2, mid = 3, botlt = 4, botrt = 5, bottom = 6
+	for _, char_a := range a {
+		overlap := false
+		for _, char_b := range b {
+			if char_a == char_b {
+				overlap = true
+			}
+		}
+
+		if !overlap {
+			diff = append(diff, char_a)
+		}
+	}
+
+	return diff
+}
+
+func reverseMap(m map[int]string) map[string]int {
+	reversed := make(map[string]int, len(m))
+
+	for k, v := range m {
+		reversed[v] = k
+	}
+
+	return reversed
+}
+
+func sortAlpha(str string) string {
+	runes := []rune(str)
+	sort.Slice(runes, func(i, j int) bool {
+		return runes[i] < runes[j]
+	})
+
+	return string(runes)
+}
