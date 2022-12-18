880b23c71edb9a6f19a4d676ddf814a7c3ca9a2e
Tommy Zarick <zarick.6@osu.edu>
2022-12-18 00:18:25 -0500

    day 7 part 1 working

---
+++ 2022/day07/solution.spec.ts
@@ -0,0 +1,84 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+import { solution } from './solution';
+
+const testCases = [
+  {
+    part: 1,
+    input: `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`,
+    expected: 95437,
+  },
+  {
+    part: 2,
+    input: `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`,
+    expected: 24933642,
+  },
+];
+
+describe('Solution Test Cases - Day 13', () => {
+  describe('Part 1', () => {
+    const part1TestCases = testCases.filter((item) => item.part === 1);
+    for (let i = 0; i < part1TestCases.length; i++) {
+      const { input, expected } = part1TestCases[i];
+
+      it(`Case ${i + 1}`, () => {
+        expect(solution(input).part1).to.equal(expected);
+      });
+    }
+  });
+
+  describe('Part 2', () => {
+    const part2TestCases = testCases.filter((item) => item.part === 2);
+    for (let i = 0; i < part2TestCases.length; i++) {
+      const { input, expected } = part2TestCases[i];
+
+      it(`Case ${i + 1}`, () => {
+        expect(solution(input).part2).to.equal(expected);
+      });
+    }
+  });
+});
---
+++ 2022/day07/solution.ts
@@ -0,0 +1,97 @@
+interface result {
+  part1: number;
+  part2: number;
+}
+
+const MAX_SMALL_DIR_SIZE = 100000;
+const TOTAL_DISK_SPACE = 70000000;
+const SPACE_NEEDED_FOR_UPDATE = 30000000;
+
+export const solution = (input: string): result => {
+  const filesystem = populateFilesystemInfo(input);
+  calculateMemoryTotals(filesystem['/']);
+  const totalSmallDirMemUsage = getSmallDirMemUsage(filesystem['/']);
+
+  return {
+    part1: totalSmallDirMemUsage,
+    part2: 0
+  };
+};
+
+const populateFilesystemInfo = (input: string) => {
+  const filesystem: Record<string, any> = {
+    '/': {}
+  };
+  const depthStack: string[] = []; // keep track of our depth traversal
+
+  const lines = input.split(/\r?\n/);
+
+  for (const line of lines) {
+    const selectedDir = line.match(/(?<=\$ cd\s).*/)?.[0];
+    if (!selectedDir) {
+      // we are reading the output of ls (we don't need to do anything for the ls cmd itself)
+      if (line !== '$ ls') {
+        const elements = line.split(/\s/);
+        const prefix = elements[0];
+
+        // iterate through to the correct level
+        let currLevel = filesystem;
+        for (const level of depthStack) {
+          currLevel = currLevel[level];
+        }
+
+        if (prefix === 'dir') { //dir
+          const dirName = elements[1];
+          currLevel[dirName] = {}; // modify by ref
+        } else { // file
+          const fileSize = parseInt(prefix);
+          const filename = elements[1];
+          currLevel[filename] = fileSize;
+        }
+      }
+    } else if (selectedDir === '..') {
+      depthStack.pop();
+    } else {
+      depthStack.push(selectedDir);
+    }
+  }
+
+  return filesystem;
+};
+
+const calculateMemoryTotals = (filesystemLevel: Record<string, any>): number => {
+  let totalMemUsage = 0;
+  for (const [key, value] of Object.entries(filesystemLevel)) {
+    if (typeof (value) === 'number') {
+      // file
+      totalMemUsage += value;
+    } else {
+      // dir
+      totalMemUsage += calculateMemoryTotals(filesystemLevel[key]);
+    }
+  }
+
+  filesystemLevel.totalMemUsage = totalMemUsage;
+  return totalMemUsage;
+};
+
+const getSmallDirMemUsage = (filesystemLevel: Record<string, any>): number => {
+  let smallDirMemUsage = 0;
+
+  if (filesystemLevel.totalMemUsage <= MAX_SMALL_DIR_SIZE) {
+    smallDirMemUsage += filesystemLevel.totalMemUsage;
+  }
+
+  for (const [key, value] of Object.entries(filesystemLevel)) {
+    if (typeof (value) === 'object') {
+      // dir
+      smallDirMemUsage += getSmallDirMemUsage(filesystemLevel[key]);
+    }
+  }
+
+  return smallDirMemUsage;
+};
+
+const findBestDirSizeToDelete = (filesystemLevel: Record<string, any>): number => {
+
+};
